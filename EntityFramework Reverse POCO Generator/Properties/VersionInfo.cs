febd58f04e50af02a68657bddc98a4c7c77dedab
Simon Hughes <simon.hughes@bybox.com>
2012-10-31 11:45:10 +0000

    FK's match tables with non 'dbo' schema.

--- EntityFramework Reverse POCO Generator/Properties/VersionInfo.cs
+++ EntityFramework Reverse POCO Generator/Properties/VersionInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("1.0.0.4")]
-[assembly: AssemblyFileVersion("1.0.0.4")]
+[assembly: AssemblyVersion("1.0.0.6")]
+[assembly: AssemblyFileVersion("1.0.0.6")]
