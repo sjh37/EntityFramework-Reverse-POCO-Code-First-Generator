b697732a10b8ce2b1cae62f552397c688c7f5dac
Simon Hughes <simon.hughes@bybox.com>
2012-11-06 16:07:37 +0000

    Changed into VS extension

--- pack.bat
+++ pack.bat
@@ -1,2 +1,9 @@
-del *.nupkg /Q
-nuget pack "EntityFramework Reverse POCO Generator\EntityFramework Reverse POCO Generator.csproj" -build -Properties Configuration=Release
+@echo off
+md VS.Extension\ItemTemplates\Data\EFRPOCO
+copy "EntityFramework Reverse POCO Generator\*.tt*" VS.Extension\ItemTemplates\Data\EFRPOCO\
+cd VS.Extension\ItemTemplates\Data
+"C:\Program Files\7-Zip\7z.exe" u EFRPOCO.zip
+del EFRPOCO /q
+rd EFRPOCO
+cd..\..
+"C:\Program Files\7-Zip\7z.exe" a EF.Reverse.POCO.zip
