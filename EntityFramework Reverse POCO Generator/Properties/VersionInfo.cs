f282dafb195c54292108cc0d9171e5ca6e6d2e77
Simon Hughes <simon.hughes@bybox.com>
2012-11-16 14:49:06 +0000

    Created ctor() containing sql default values.

--- EntityFramework Reverse POCO Generator/Properties/VersionInfo.cs
+++ EntityFramework Reverse POCO Generator/Properties/VersionInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("1.0.0.33")]
-[assembly: AssemblyFileVersion("1.0.0.33")]
+[assembly: AssemblyVersion("1.0.0.35")]
+[assembly: AssemblyFileVersion("1.0.0.35")]
