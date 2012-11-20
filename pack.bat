f73e3f9bac4a37f1cb614012954855e7021215c6
Simon Hughes <simon.hughes@bybox.com>
2012-11-20 12:26:56 +0000

    Creating the Visual studio item template.

--- pack.bat
+++ pack.bat
@@ -1,9 +1,6 @@
 @echo off
-md VS.Extension\ItemTemplates\Data\EFRPOCO
-copy "EntityFramework Reverse POCO Generator\*.tt*" VS.Extension\ItemTemplates\Data\EFRPOCO\
-cd VS.Extension\ItemTemplates\Data
-"C:\Program Files\7-Zip\7z.exe" u EFRPOCO.zip
-del EFRPOCO /q
-rd EFRPOCO
-cd..\..
+md VS.Extension\ItemTemplates\Data
+copy "EntityFramework Reverse POCO Generator\*.tt*" VS.Extension\ItemTemplates\Data
+cd VS.Extension
+del *.zip
 "C:\Program Files\7-Zip\7z.exe" a EF.Reverse.POCO.zip
