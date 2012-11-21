db1b6611b771d78f84fd8077608076a53adc2396
Simon Hughes <simon.hughes@bybox.com>
2012-11-21 16:53:35 +0000

    Finalised development for v1.0.0

--- EntityFramework Reverse POCO Generator/Properties/VersionInfo.base.cs
+++
@@ -1,3 +0,0 @@
-using System.Reflection;
-[assembly: AssemblyVersion("1.0.0.$REVNUM$")]
-[assembly: AssemblyFileVersion("1.0.0.$REVNUM$")]
--- EntityFramework Reverse POCO Generator/Properties/VersionInfo.cs
+++
@@ -1,3 +0,0 @@
-using System.Reflection;
-[assembly: AssemblyVersion("1.0.0.49")]
-[assembly: AssemblyFileVersion("1.0.0.49")]
