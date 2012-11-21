f4606ae41e34f21efbf02dfdedbca52035f98de4
Simon Hughes <simon.hughes@bybox.com>
2012-11-21 13:36:57 +0000

    Creating item template manifest

--- pack.bat
+++ pack.bat
@@ -1,5 +1,10 @@
 @echo off
-copy "EntityFramework Reverse POCO Generator\*.tt*" ItemTemplate\
-cd ItemTemplate
+copy "EntityFramework Reverse POCO Generator\*.tt*" ItemTemplates\Data\
+copy "EntityFramework Reverse POCO Generator\*.txt" ItemTemplates\Data
+cd ItemTemplates\Data
 del *.zip
+"C:\Program Files\7-Zip\7z.exe" a "efrpoco.zip"
+del *.t*
+cd..\..
 "C:\Program Files\7-Zip\7z.exe" a "EntityFramework Reverse POCO Generator 1.0.0.zip"
+rem ren "EntityFramework Reverse POCO Generator 1.0.0.zip" "EntityFramework Reverse POCO Generator 1.0.0.vsix"
