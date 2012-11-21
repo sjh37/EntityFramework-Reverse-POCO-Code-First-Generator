725e1e19d10764c7c289d8b7ecbde0ea1a7dea0a
Simon Hughes <simon.hughes@bybox.com>
2012-11-21 11:24:32 +0000

    Created the item template

--- pack.bat
+++ pack.bat
@@ -1,6 +1,5 @@
 @echo off
-md VS.Extension\ItemTemplates\Data
-copy "EntityFramework Reverse POCO Generator\*.tt*" VS.Extension\ItemTemplates\Data
-cd VS.Extension
+copy "EntityFramework Reverse POCO Generator\*.tt*" ItemTemplate\
+cd ItemTemplate
 del *.zip
-"C:\Program Files\7-Zip\7z.exe" a EF.Reverse.POCO.zip
+"C:\Program Files\7-Zip\7z.exe" a "EntityFramework Reverse POCO Generator 1.0.0.zip"
