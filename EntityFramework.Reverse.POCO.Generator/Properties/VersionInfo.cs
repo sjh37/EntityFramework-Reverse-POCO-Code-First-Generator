babdeef2651957bfe79d9313951896ca209ef437
Simon Hughes <simon.hughes@bybox.com>
2013-06-05 14:32:48 +0100

    Update revision number to 1.7.1

--- EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
+++ EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("1.7.0.90")]
-[assembly: AssemblyFileVersion("1.7.0.90")]
+[assembly: AssemblyVersion("1.7.1.95")]
+[assembly: AssemblyFileVersion("1.7.1.95")]
