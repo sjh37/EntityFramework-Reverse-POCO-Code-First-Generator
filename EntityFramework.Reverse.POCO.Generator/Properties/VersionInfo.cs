fe9de1954cc2c2f607c14b365569bce3103d68d5
Simon Hughes <simon.hughes@bybox.com>
2013-09-10 10:43:06 +0100

    Updated revision to 1.12 for release.

--- EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
+++ EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("1.11.0.120")]
-[assembly: AssemblyFileVersion("1.11.0.120")]
+[assembly: AssemblyVersion("1.12.0.130")]
+[assembly: AssemblyFileVersion("1.12.0.130")]
