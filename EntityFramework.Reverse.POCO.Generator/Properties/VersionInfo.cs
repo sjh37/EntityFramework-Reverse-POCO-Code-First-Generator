4f5e696b4a9ae0bf9eb23080c933b958e0f77077
Simon Hughes <simon.hughes@bybox.com>
2013-11-13 22:04:26 +0000

    Set revision to v2.1.0

--- EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
+++ EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("2.0.0.148")]
-[assembly: AssemblyFileVersion("2.0.0.148")]
+[assembly: AssemblyVersion("2.1.0.154")]
+[assembly: AssemblyFileVersion("2.1.0.154")]
