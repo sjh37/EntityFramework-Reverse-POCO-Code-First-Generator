3c88cdb7e230555c448198c81744d48200dafa1f
Simon Hughes <simon.hughes@bybox.com>
2014-01-10 12:14:32 +0000

    Upgraded to EntityFramework v6.0.2

--- EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
+++ EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("2.1.0.161")]
-[assembly: AssemblyFileVersion("2.1.0.161")]
+[assembly: AssemblyVersion("2.2.0.163")]
+[assembly: AssemblyFileVersion("2.2.0.163")]
