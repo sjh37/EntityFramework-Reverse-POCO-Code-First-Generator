878b11891ccd18c459a7c18b341424e820ea18c8
Simon Hughes <simon.hughes@bybox.com>
2014-01-17 10:49:01 +0000

    Removed code that is no longer required.

--- EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
+++ EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("2.2.0.177")]
-[assembly: AssemblyFileVersion("2.2.0.177")]
+[assembly: AssemblyVersion("2.2.0.178")]
+[assembly: AssemblyFileVersion("2.2.0.178")]
