f8af8e99d04ee56b247f8c926d3edc4e24d0c85a
Simon Hughes <simon.hughes@bybox.com>
2014-02-05 12:31:11 +0000

    Build release for 2.3.2

--- EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.base.cs
+++ EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.base.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("2.3.1.$REVNUM$")]
-[assembly: AssemblyFileVersion("2.3.1.$REVNUM$")]
+[assembly: AssemblyVersion("2.3.2.$REVNUM$")]
+[assembly: AssemblyFileVersion("2.3.2.$REVNUM$")]
