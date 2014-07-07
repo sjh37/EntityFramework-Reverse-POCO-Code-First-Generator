9b3213f367256e30642d58b2c6b75c73992257cd
Simon Hughes <simon.hughes@bybox.com>
2014-07-07 17:19:36 +0100

    Created a new VS2013 VSIX project to replace the old one.

--- EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
+++ EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("2.5.1.222")]
-[assembly: AssemblyFileVersion("2.5.1.222")]
+[assembly: AssemblyVersion("2.5.1.223")]
+[assembly: AssemblyFileVersion("2.5.1.223")]
