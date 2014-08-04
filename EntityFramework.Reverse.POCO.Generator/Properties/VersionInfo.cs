d2a129b29d05d30425a491b2e88b5a1421475ca6
Simon Hughes <simon.hughes@bybox.com>
2014-08-04 09:16:48 +0100

    Add extended comments for SqlCE

--- EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
+++ EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("2.6.0.224")]
-[assembly: AssemblyFileVersion("2.6.0.224")]
+[assembly: AssemblyVersion("2.6.1.226")]
+[assembly: AssemblyFileVersion("2.6.1.226")]
