571ddee47420255771edb894aad1054654f71ded
Simon Hughes <simon.hughes@bybox.com>
2014-10-24 15:30:59 +0100

    Updated Nuget packages.

--- EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
+++ EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("2.8.1.249")]
-[assembly: AssemblyFileVersion("2.8.1.249")]
+[assembly: AssemblyVersion("2.8.1.254")]
+[assembly: AssemblyFileVersion("2.8.1.254")]
