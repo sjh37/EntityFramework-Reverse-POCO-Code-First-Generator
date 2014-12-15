fbb758cca7553ba5ff288ef6432c77be7b22d4ea
Simon Hughes <simon.hughes@bybox.com>
2014-12-15 11:29:54 +0000

    Include examples of CustomPluralizationEntry. Thanks to Carrie Kroutil

--- EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
+++ EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("2.8.3.275")]
-[assembly: AssemblyFileVersion("2.8.3.275")]
+[assembly: AssemblyVersion("2.8.3.278")]
+[assembly: AssemblyFileVersion("2.8.3.278")]
