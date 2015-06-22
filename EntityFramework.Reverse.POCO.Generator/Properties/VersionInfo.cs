b5d245075d85002a552d10813e7ef8e0664931ba
Simon Hughes <simon.hughes@bybox.com>
2015-06-22 23:52:56 +0100

    Ready for release

--- EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
+++ EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("2.14.0.409")]
-[assembly: AssemblyFileVersion("2.14.0.409")]
+[assembly: AssemblyVersion("2.14.0.419")]
+[assembly: AssemblyFileVersion("2.14.0.419")]
