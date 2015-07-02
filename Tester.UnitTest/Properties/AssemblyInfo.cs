ee65d74ca3c50c1117d28bbd63073045c494e24b
Simon Hughes <simon.hughes@bybox.com>
2015-07-02 10:13:28 +0100

    Change copyright symbol to text as this uses western european encoding.

--- Tester.UnitTest/Properties/AssemblyInfo.cs
+++ Tester.UnitTest/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Tester.UnitTest")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright (C) Simon Hughes 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
