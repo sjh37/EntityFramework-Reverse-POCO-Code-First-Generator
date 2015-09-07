d52c87fae58e5dc539fb9d99acbf9f0bc04da322
Simon Hughes <simon.hughes@bybox.com>
2015-09-07 11:03:22 +0100

    Add use of [DbFunctionDetails] attribute to specify schema name for TVF functions.

--- EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
+++ EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("2.15.1.459")]
-[assembly: AssemblyFileVersion("2.15.1.459")]
+[assembly: AssemblyVersion("2.15.1.468")]
+[assembly: AssemblyFileVersion("2.15.1.468")]
