5a2595e5a6593b9364b079ab808c1a5c78b7cebc
Simon Hughes <simon.hughes@bybox.com>
2015-09-07 11:52:32 +0100

    Revision bump

--- EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
+++ EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("2.15.2.471")]
-[assembly: AssemblyFileVersion("2.15.2.471")]
+[assembly: AssemblyVersion("2.15.2.474")]
+[assembly: AssemblyFileVersion("2.15.2.474")]
