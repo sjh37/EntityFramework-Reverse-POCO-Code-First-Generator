66e1ac73c687e4239cdd641015bcf508e2a423be
Simon Hughes <simon.hughes@bybox.com>
2016-02-09 14:20:25 +0000

    Case 172. Minimise usings to the bare minimum required.

--- EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
+++ EntityFramework.Reverse.POCO.Generator/Properties/VersionInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("2.17.2.535")]
-[assembly: AssemblyFileVersion("2.17.2.535")]
+[assembly: AssemblyVersion("2.17.2.543")]
+[assembly: AssemblyFileVersion("2.17.2.543")]
