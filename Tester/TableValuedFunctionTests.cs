1ec7d46527c05d302ffd3b1c0f96554fb9179f95
Tim Sirmovics <tim.sirmovics@gmail.com>
2018-01-16 09:45:37 +1100

    Added tests exposing function referenced through interface exception.

--- Tester/TableValuedFunctionTests.cs
+++ Tester/TableValuedFunctionTests.cs
@@ -22,6 +22,21 @@ namespace Tester
         }
 
         [Test]
+        public void Standard_FunctionCanBeCalledWithinQueryThroughInterfaceReference()
+        {
+            // Arrange
+            var db = (TestDatabaseStandard.ITestDbContext)new TestDatabaseStandard.TestDbContext();
+
+            // Act
+            var data = (from f in db.Stafford_Foos let i = db.CsvToInt("123,456", "") select i).SelectMany(i => i).ToList();
+
+            // Assert
+            Assert.AreEqual(2, data.Count);
+            Assert.AreEqual(123, data[0].IntValue);
+            Assert.AreEqual(456, data[1].IntValue);
+        }
+
+        [Test]
         public void DataAnnotation_test()
         {
             // Arrange
@@ -29,6 +44,21 @@ namespace Tester
 
             // Act
             var data = db.CsvToInt("123,456", "").ToList();
+
+            // Assert
+            Assert.AreEqual(2, data.Count);
+            Assert.AreEqual(123, data[0].IntValue);
+            Assert.AreEqual(456, data[1].IntValue);
+        }
+
+        [Test]
+        public void DataAnnotation_FunctionCanBeCalledWithinQueryThroughInterfaceReference()
+        {
+            // Arrange
+            var db = (TestDatabaseDataAnnotation.ITestDbContext)new TestDatabaseDataAnnotation.TestDbContext();
+
+            // Act
+            var data = (from f in db.Stafford_Foos let i = db.CsvToInt("123,456", "") select i).SelectMany(i => i).ToList();
 
             // Assert
             Assert.AreEqual(2, data.Count);
