20d162d6128aece4e670d0ba08bfd75108a2c5b2
Tim Sirmovics <tim.sirmovics@gmail.com>
2018-01-16 10:11:47 +1100

    Update tests.

--- Tester/TableValuedFunctionTests.cs
+++ Tester/TableValuedFunctionTests.cs
@@ -28,12 +28,10 @@ namespace Tester
             var db = (TestDatabaseStandard.ITestDbContext)new TestDatabaseStandard.TestDbContext();
 
             // Act
-            var data = (from f in db.Stafford_Foos let i = db.CsvToInt("123,456", "") select i).SelectMany(i => i).ToList();
+            var data = (from f in db.Stafford_Foos let i = db.CsvToInt("123,456", "") select new {f, i}).ToList().SelectMany(d => d.i).ToList();
 
             // Assert
-            Assert.AreEqual(2, data.Count);
-            Assert.AreEqual(123, data[0].IntValue);
-            Assert.AreEqual(456, data[1].IntValue);
+            Assert.AreEqual(4, data.Count);
         }
 
         [Test]
@@ -58,12 +56,10 @@ namespace Tester
             var db = (TestDatabaseDataAnnotation.ITestDbContext)new TestDatabaseDataAnnotation.TestDbContext();
 
             // Act
-            var data = (from f in db.Stafford_Foos let i = db.CsvToInt("123,456", "") select i).SelectMany(i => i).ToList();
+            var data = (from f in db.Stafford_Foos let i = db.CsvToInt("123,456", "") select new { f, i }).ToList().SelectMany(d => d.i).ToList();
 
             // Assert
-            Assert.AreEqual(2, data.Count);
-            Assert.AreEqual(123, data[0].IntValue);
-            Assert.AreEqual(456, data[1].IntValue);
+            Assert.AreEqual(4, data.Count);
         }
     }
 }
