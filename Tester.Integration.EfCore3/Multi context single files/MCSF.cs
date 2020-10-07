000ad68b52996ae6b13602401c3c358662925eb8
Simon Hughes <simon.hughes@zapinamo.com>
2020-10-07 09:56:31 +0100

    Remove initial blank line

--- Tester.Integration.EfCore3/Multi context single files/MCSF.cs
+++ Tester.Integration.EfCore3/Multi context single files/MCSF.cs
@@ -1,7 +1,1 @@
 ﻿
-// -----------------------------------------------------------------------------------------
-// Failed to generate the code in GenerateCode() - Object reference not set to an instance of an object.
-/*   at Microsoft.VisualStudio.TextTemplatingB3B97CCC1D2849083AC512DC8F909223459FB7523A724BB68CEBF90868A805E17B69CA876784D5873CDB57F935D25C54A016C966558C8A172FE5AB447016E5FD.GeneratedTextTransformation.Generator.GenerateCode(IDbContextFilter filter) in C:\S\Source (open source)\EntityFramework Reverse POCO Code Generator\Tester.Integration.EfCore3\Multi context single files\..\..\EntityFramework.Reverse.POCO.Generator\EF.Reverse.POCO.v3.ttinclude:line 4731
-   at Microsoft.VisualStudio.TextTemplatingB3B97CCC1D2849083AC512DC8F909223459FB7523A724BB68CEBF90868A805E17B69CA876784D5873CDB57F935D25C54A016C966558C8A172FE5AB447016E5FD.GeneratedTextTransformation.Generator.GenerateCode() in C:\S\Source (open source)\EntityFramework Reverse POCO Code Generator\Tester.Integration.EfCore3\Multi context single files\..\..\EntityFramework.Reverse.POCO.Generator\EF.Reverse.POCO.v3.ttinclude:line 4658*/
-// -----------------------------------------------------------------------------------------
-
