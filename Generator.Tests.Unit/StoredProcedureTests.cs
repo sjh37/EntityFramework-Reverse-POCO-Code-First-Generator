1e04bd49c9d6288a817d8e1e6c65022416bce2b7
Robert Angers <robert.angers@gmail.com>
2021-01-01 09:32:49 -0500

    Issue #286

---
+++ Generator.Tests.Unit/StoredProcedureTests.cs
@@ -0,0 +1,40 @@
+﻿using Efrpg;
+using NUnit.Framework;
+using System.Data;
+
+namespace Generator.Tests.Unit
+{
+    [TestFixture]
+    public class StoredProcedureTests
+    {
+        public StoredProcedure Sut { get; private set; }
+
+        [SetUp]
+        public void Init()
+        {
+            Sut = new StoredProcedure { Schema = new Schema("dbo"), DbName = "name", NameHumanCase = "some_sp", IsStoredProcedure = true };
+
+        }
+
+        [Description("Issue #286")]
+        [Test]
+        [TestCase("JSON_F52E2B61-18A1-11d1-B105-00805F49916B", "JSON_Value")]
+        [TestCase("XML_F52E2B61-18A1-11d1-B105-00805F49916B", "XML_Value")]
+        public void ColumnNameForXmlOrJsonReturnType(string exampleServerGenerated, string expected)
+        {
+            // Arrange
+
+            var col = new DataColumn
+            {
+                DataType = typeof(string),
+                ColumnName = exampleServerGenerated,
+            };
+
+            // Act
+            var result = Sut.WriteStoredProcReturnColumn(col);
+
+            // Assert
+            Assert.AreEqual($"public string {expected} {{ get; set; }}", result);
+        }
+    }
+}
