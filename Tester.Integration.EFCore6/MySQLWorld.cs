ab9162d23bc0bc4cdfc25467b6bbee7d15ab6cb9
Simon Hughes <simon@hicrest.net>
2022-08-08 22:25:18 +0100

    MySQL Db creation

---
+++ Tester.Integration.EFCore6/MySQLWorld.cs
@@ -0,0 +1,17 @@
+﻿
+
+// ------------------------------------------------------------------------------------------------
+// WARNING: Failed to load provider "MySql.Data.MySqlClient" - Unable to find the requested .Net Framework Data Provider.  It may not be installed.
+// Allowed providers:
+//    "System.Data.Odbc"
+//    "System.Data.OleDb"
+//    "System.Data.OracleClient"
+//    "System.Data.SqlClient"
+//    "System.Data.SqlServerCe.4.0"
+//    "Npgsql"
+//    "Microsoft.SqlServerCe.Client.4.0"
+
+/*   at System.Data.Common.DbProviderFactories.GetFactory(String providerInvariantName)
+   at Microsoft.VisualStudio.TextTemplating77B348FB79B2579717C864F11B3CA6ED94C23E7A8525D08694B9C5F938F1E6439FC5582C3D01BAC6548FA3CD8CC1DC2E541B0BC516137592998CD1D9AA5551AB.GeneratedTextTransformation.GeneratorFactory.Create(FileManagementService fileManagementService, Type fileManagerType, String singleDbContextSubNamespace) in C:\S\Source (open source)\EntityFramework Reverse POCO Code Generator\Tester.Integration.EFCore6\..\EntityFramework.Reverse.POCO.Generator\EF.Reverse.POCO.v3.ttinclude:line 6306*/
+// ------------------------------------------------------------------------------------------------
+
