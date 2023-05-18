b96b9d5e301c3c51925dd225f62df476837bcc01
Simon Hughes <simon@hicrest.net>
2023-05-18 22:52:58 +0100

    SQLite testing

--- EntityFramework.Reverse.POCO.Generator/Database.cs
+++ EntityFramework.Reverse.POCO.Generator/Database.cs
@@ -1,1 +1,1 @@
-﻿// Please update your .tt file's Settings.ConnectionString string to the database you want to reverse engineer and save your .tt file.
+﻿// Please update your .tt file's `Settings.ConnectionString` string to the database you want to reverse engineer and save your .tt file.
