e33e9cc0202959819847a2a74322cce41a54854d
Simon Hughes <simon@hicrest.net>
2024-03-31 22:52:08 +0100

    comment alignment

--- BuildTT/BuildTT.cs
+++ BuildTT/BuildTT.cs
@@ -157,7 +157,7 @@ namespace BuildTT
             Table      = ""EnumTest.DaysOfWeek"", // Database table containing enum values. e.g. ""DaysOfWeek""
             NameField  = ""TypeName"",            // Column containing the name for the enum. e.g. ""TypeName""
             ValueField = ""TypeId"",              // Column containing the values for the enum. e.g. ""TypeId""
-            GroupField = string.Empty             // [optional] Column containing the group name for the enum. This is used if multiple Enums are in the same table. if this is populated, use {GroupField} in the Name property. e.g. ""{GroupField}Enum""
+            GroupField = string.Empty           // [optional] Column containing the group name for the enum. This is used if multiple Enums are in the same table. if this is populated, use {GroupField} in the Name property. e.g. ""{GroupField}Enum""
         },
         new EnumerationSettings
         {
