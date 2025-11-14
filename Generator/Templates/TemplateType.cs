e29e14efba3cd17e8e0c07d3d15352e524a90ac3
Bryan Tichy <bryantichy@infinexgroup.com>
2025-11-14 15:16:20 -0500

    Add EF Core 10 support with SQL Server 2025 type mapping
    - Added TemplateType.EfCore10 and FileBasedCore10 enum values
    - Created TemplateEfCore10 class inheriting from TemplateEfCore8
    - Updated TemplateFactory to route EfCore10 template types
    - Added IsEfCore10Plus() helper method in Settings
    - Implemented AutoMapSqlServer2025Types feature flag
    - Added automatic vector and json type mapping for SQL Server 2025
    - Created Tester.Integration.EFCore10 project targeting .NET 10
    - Updated BuildTT to generate EFCore10 template files
    - Created Templates.EFCore10 folder structure
    
    Resolves #869

--- Generator/Templates/TemplateType.cs
+++ Generator/Templates/TemplateType.cs
@@ -5,8 +5,10 @@
         Ef6,
         EfCore8,
         EfCore9,
+        EfCore10,
         FileBasedEf6,
         FileBasedCore8,
-        FileBasedCore9
+        FileBasedCore9,
+        FileBasedCore10
     }
 }
