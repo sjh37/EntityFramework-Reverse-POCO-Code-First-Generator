ee6a9f9e04a2fdf609c3f7fe6a3c2d7f79b83e23
Simon Hughes <simon@hicrest.net>
2026-01-02 19:54:25 +0000

    Add efcore 10 specific types (VECTOR, JSON)

---
+++ ReversePoco.sql
@@ -0,0 +1,8 @@
+﻿-- EFCore 10 specific types: vector and json
+CREATE TABLE Testing_EfCore10
+(
+    Id INT NOT NULL IDENTITY(1, 1),
+    Embedding vector(1234) NULL,
+    ShippingAddress json NOT NULL,
+    CONSTRAINT PK_Testing_EfCore10 PRIMARY KEY CLUSTERED (Id)
+);
